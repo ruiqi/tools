51ce1e4e311d9de908f48ca799907ad86be5b490
liruiqi <liruiqi@wandoujia.com>
2014-06-25 20:38:36 +0800

    rm sudo

--- install_python2.7/install.sh
+++ install_python2.7/install.sh
@@ -8,13 +8,13 @@ wget -c https://pypi.python.org/packages/source/s/setuptools/setuptools-5.2.tar.
 wget -c https://pypi.python.org/packages/source/p/pip/pip-1.5.6.tar.gz --no-check-certificate
 
 tar xzvf Python-2.7.7.tgz
-(cd Python-2.7.7 && ./configure --prefix=$LOCAL && sudo make && sudo make install)
-sudo rm -rf Python-2.7.7
+(cd Python-2.7.7 && ./configure --prefix=$LOCAL && make && make install)
+rm -rf Python-2.7.7
 
 tar xzvf setuptools-5.2.tar.gz
-(cd setuptools-5.2 && sudo $LOCAL/bin/python setup.py install)
-sudo rm -rf setuptools-5.2
+(cd setuptools-5.2 && $LOCAL/bin/python setup.py install)
+rm -rf setuptools-5.2
 
 tar xzvf pip-1.5.6.tar.gz
-(cd pip-1.5.6 && sudo $LOCAL/bin/python setup.py install)
-sudo rm -rf pip-1.5.6
+(cd pip-1.5.6 && $LOCAL/bin/python setup.py install)
+rm -rf pip-1.5.6
